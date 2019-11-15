7af5e55f43f83ba24b7abf3989f541ebb7fcb15c
FilippoRanza <filipporanza@gmail.com>
2019-11-15 14:18:13 +0100

    this commit fails the tests...

--- src/create_path/dijkstra.rs
+++ src/create_path/dijkstra.rs
@@ -7,33 +7,66 @@ use ndarray::prelude::Array2;
 
 pub fn shortest_path(graph: &Array2<f64>, from: usize, to: usize) -> Option<Vec<usize>> {
 
-    let mut output = Vec::new();
     let mut curr = from;
+    let mut path = HashMap::new();
 
     let mut dist = initialize_distances(graph, from);
     
     while curr != to {
-
+        match dist.find_min() {
+            Some(next) => {
+                path.insert(next, curr);
+                find_distances(graph, next, curr, &mut dist);
+                curr = next;
+            },
+            None =>{
+                println!("{:?}", path);
+                return None;
+                }
+        }
     }
 
-    Some(output)
+    Some(build_path(path, from, to))
 }
 
+fn find_distances(graph: &Array2<f64>, next: usize, prev: usize, dist: &mut Distances) {
+    let row = graph.row(next);
+    let curr = dist.get_value(prev).unwrap();
+   
+    for (i, v) in row.iter().enumerate().filter(|(_, v)|  **v != 0.0) {
+        let tmp = *v + curr;
+        println!("{:?}", tmp);
+        dist.set_dist(i, tmp);
+    }
+}
 
 fn initialize_distances(graph: &Array2<f64>, origin: usize) -> Distances {
     let len = graph.shape()[0];
     let mut dist = Distances::new(len, origin);
     let row = graph.row(origin);
     for (i, v) in row.iter().enumerate().filter(|(_, v)|  **v != 0.0) {
-        println!("{:?} {:?}", i, v);
-        dist.fix_distance(i, *v);
+        dist.set_dist(i, *v);
     }
 
     dist
 }
 
+fn build_path(tree: HashMap<usize, usize>, from: usize, to: usize) -> Vec<usize> {
+    let mut out = Vec::new();
+
+    let mut curr = to;
+    while curr != from {
+        let prev = tree.get(&curr).unwrap();
+        out.push(curr);
+        curr = *prev;
+    }
+
+    out
+}
+
 struct Distances {
     dist: HashMap<usize, Value>,
+    start: usize,
 }
 
 impl Distances {
@@ -44,7 +77,7 @@ impl Distances {
                 out.insert(i, Value::new());     
             }
         } 
-        Distances{ dist: out}
+        Distances{ dist: out, start: origin}
     }
 
     fn find_min(&mut self) -> Option<usize> {
@@ -89,14 +122,13 @@ impl Distances {
         }
     }
 
-    fn fix_distance(&mut self, node: usize, dist: f64) {
-        let value = self.dist.get_mut(&node).unwrap();
-        value.fix = true;
-        value.val = Some(dist);
-    }
-
     fn get_value(&self, k: usize) -> Option<f64> {
-        self.dist.get(&k).unwrap().val
+        if k == self.start {
+            Some(0.0)
+        }
+        else {
+            self.dist.get(&k).unwrap().val
+        }
     }
 
 }
@@ -111,6 +143,8 @@ impl Value {
         Value{val: None, fix: false}
     }
 }
+
+
 
 
 
@@ -147,17 +181,6 @@ mod test{
             Some(_) => assert!(false),
             None => assert!(true)
         };
-
-    
-        dist.fix_distance(2, 1.2);
-        match dist.find_min() {
-            Some(_) => assert!(false),
-            None => assert!(true)
-        };
-
-        let val = dist.dist.get(&(2 as usize)).unwrap();
-        assert_eq!(val.val, Some(1.2));
-        assert!(val.fix)
 
     }
 
@@ -202,8 +225,25 @@ mod test{
                 None => assert!(true)
             }
         }
+    }
 
 
+    #[test]
+    fn test_shortest_path() {
+        let graph = array![[0.0, 9.0, 6.0, 0.0, 0.0, 0.0, 0.0],
+                           [9.0, 0.0, 11.0, 1.0, 20.0, 0.0, 0.0],
+                           [6.0, 11.0, 0.0, 4.0, 0.0, 18.0, 0.0],
+                           [0.0, 1.0, 2.0, 0.0, 13.0, 28.0, 15.0],
+                           [0.0, 20.0, 0.0, 13.0, 0.0, 0.0, 3.0],
+                           [0.0, 0.0, 18.0, 28.0, 0.0, 0.0, 25.0],
+                           [0.0, 0.0, 0.0, 15.0, 3.0, 25.0, 0.0]];
+
+
+        let path = shortest_path(&graph, 0, 5);
+        match path {
+            Some(_) => assert!(true),
+            None => assert!(false)
+        };
     }
 
 }
